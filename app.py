79932c9e97f04623844dc8a5b3bf1bb1a3abd01c
LG <vidushbakshi96@gmail.com>
2018-09-28 12:18:01 +0000

    Upload New File

---
+++ app.py
@@ -0,0 +1,27 @@
+
+from flask import Flask,render_template,request
+import os
+
+from Digi_Class import digit
+
+app=Flask(__name__)
+
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    return render_template('digit.html')
+
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    try:
+        imgData = request.get_data()
+        out = digit(imgData)
+        li=str(out)
+        return li
+    except Exception as e:
+        return (str(e))
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
